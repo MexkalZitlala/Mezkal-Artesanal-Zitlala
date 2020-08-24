c49f1e561ef1232f8e1cacba3dbacda2e98ea112
MexkalZitlala <70168370+MexkalZitlala@users.noreply.github.com>
2020-08-24 20:53:35 +0200

    Add files via upload

---
+++ imagen/coneccion.php
@@ -0,0 +1,11 @@
+<?php 
+$coneccion = new mysqli("localhost","root","1234","imagen");
+
+if{$conexion){
+    echo "Conexion exitosa";
+}
+else{
+    echo"Conecxion no exitosa";
+}
+}
+?>
---
+++ imagen/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Index de imagenes</title>
+</head>
+<body>
+  <center>
+      <form action="#" method="POST" enctype="multipart/form-data">
+          <input type="text"name="nombre" placeholder="nombre...."vale=""/>
+          <input type="file"name="imagen"/>
+          <input type="submit"value="aceptar">
+
+</body> 
